3ae619a7b752ec3ec2d8e666ed63ac47d3e01b30
unknown <kson@.aaa.ad.ea.com>
2010-01-29 17:42:17 +0900

    specify handle to kill in command line

--- kill_lock.py
+++ kill_lock.py
@@ -1,16 +1,25 @@
 #requires: "Sysinternals' Handle"(http://technet.microsoft.com/en-us/sysinternals/bb896655.aspx)
-import os
-pdbs=[
-    'a.pdb',
-    'b.pdb',
-    'c.pdb',
-    ]
-for pdb in pdbs:
-    a=os.popen('handle %s'%pdb)
-    for i in a.readlines():
-    if 'devenv.exe' in i:
-        j=i.split(':')
-        numbers=j[1]
-        (pid,handle)=numbers.strip().split(' ',1)
-        os.system('handle -c %s -p %s -y'%(handle.strip(), pid.strip()))
+import os, sys
+
+def kill(handle):
+	a=os.popen('handle %s'%handle)
+	for i in a.readlines():
+		if 'devenv.exe' in i:
+			j=i.split(':')
+			numbers=j[1]
+			(pid,handle)=numbers.strip().split(' ',1)
+			os.system('handle -c %s -p %s -y'%(handle.strip(), pid.strip()))
+
+if __name__ == '__main__':
+	handles=[]
+	if 1 < len(sys.argv):
+		for i in sys.argv[1:]:
+			handles.append(i)
+	else:
+		handles.append('pre_defined_a.pdb')
+		handles.append('pre_defined_b.pdb')
+		handles.append('pre_defined_c.pdb')
+
+	for handle in handles:
+		kill(handle)
 
